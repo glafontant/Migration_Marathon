4a458f11fa423840722e91d7dd9cf82968f925dd
Gabriel Lafontant <glafonta@gmail.com>
2013-09-08 20:13:30 -0400

    added column to remove favorites

---
+++ db/migrate/20130909000821_remove_favorites.rb
@@ -0,0 +1,9 @@
+class RemoveFavorites < ActiveRecord::Migration
+  def up
+    remove_column :books, :favorites
+  end
+
+  def down
+    add_column :books, :favorites, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909000104) do
+ActiveRecord::Schema.define(version: 20130909000821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20130909000104) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
-    t.boolean  "favorites"
     t.integer  "rating"
   end
 
